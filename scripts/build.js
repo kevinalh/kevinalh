7951f9271e68b67a23412b2bcb83cc964b276438
Kevin <kevinalh@hotmail.com>
2018-05-16 20:30:53 -0500

    Test 1

---
+++ scripts/build.js
@@ -0,0 +1,9 @@
+/**
+ * Build script for the static site.
+ */
+
+// AsciiDoc parser
+import * as asciidoctor from 'asciidoctor.js';
+// Extension to the Node File System (https://nodejs.org/api/fs.html)
+import * as fs from 'fs-extra';
+
