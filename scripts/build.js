ebd9339b9927eb494247e145656ed26b864cf6ee
Kevin Languasco <kevin@haystack.tv>
2023-12-31 11:28:53 -0500

    chore: remove old content from personal profile repository

--- scripts/build.js
+++
@@ -1,9 +0,0 @@
-/**
- * Build script for the static site.
- */
-
-// AsciiDoc parser
-import * as asciidoctor from 'asciidoctor.js';
-// Extension to the Node File System (https://nodejs.org/api/fs.html)
-import * as fs from 'fs-extra';
-
